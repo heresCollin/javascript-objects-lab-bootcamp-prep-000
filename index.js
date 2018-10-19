1ea705e26dc3806ba50718dc8d8c35eb51d3650a
Collin Bradley <herescollin@gmail.com>
2018-10-19 18:01:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ function updateObjectWithKeyAndValue(object, key, value){
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return Object.assign(object,{[key]:value})
 }
+
+function deleteFromObjectByKey(object, key){
+  var newObject = object
+  delete newObject[key]
+  newObject
+}
